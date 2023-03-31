941852f1ceae6888dcd9f14680aab0cac8726dc8
berat03 <bbulbulwork@gmail.com>
2023-03-31 22:25:50 +0100

    Working webscraper to get occupancy

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-#
+from bs4 import BeautifulSoup
+import requests
+
+url = 'https://apps.dur.ac.uk/study-spaces/library/bill-bryson/occupancy/display'
+soup = BeautifulSoup(requests.get(url).text, 'html.parser')
+count = soup.find('text', {'class': 'donut-text'}).string
+count = count.string.strip()
+count = count.replace(',', '')
+print(count)
