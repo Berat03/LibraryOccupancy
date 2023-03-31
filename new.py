9f9933a38a2bb31ce13d3e44a38678cc189e894e
berat03 <bbulbulwork@gmail.com>
2023-03-31 23:30:47 +0100

    Connected database to webscraper, adding new instances

--- new.py
+++ new.py
@@ -1,14 +1,22 @@
 import sqlite3
+from main import Occupants, event
 
 conn = sqlite3.connect('occupancy.db')
 c = conn.cursor()
+#
+# c.execute("""CREATE TABLE OCCUPANCY (
+#     date DATE,
+#     time TIME,
+#     count INTEGER
+#     )""")
 
-c.execute("""CREATE TABLE OCCUPANCY (
-    date DATE,
-    time TIME,
-    count INTEGER  
-    )""")
+c.execute("INSERT INTO OCCUPANCY VALUES (:date, :time, :count)",
+          {'date': event.date, 'time': event.time, 'count': event.count})
 
-conn.commit() # don't forget to commit changes
 
-conn.close() # good practise
+c.execute("SELECT * FROM OCCUPANCY")
+
+print(c.fetchall())
+
+conn.commit()  # don't forget to commit changes
+conn.close()  # good practise
